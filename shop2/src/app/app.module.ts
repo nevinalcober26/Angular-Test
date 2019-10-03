8f2388aa3288605e039e1a062d6e1cac8d5cef36
Nevin <nevinvinz@gmail.com>
2019-10-03 16:43:48 +0400

    Updated the Angular

--- shop2/src/app/app.module.ts
+++ shop2/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { AboutComponent } from './home/about/about.component';
+import { ServicesComponent } from './home/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BannerComponent,
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
